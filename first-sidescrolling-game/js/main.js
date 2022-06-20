ec32a12547c6954278f39d70d38ae04705683cd2
DiyanPv <diyanpetrov61@gmail.com>
2022-06-20 13:11:02 +0300

    Change movement speed variable, add position controls and move-borders

--- first-sidescrolling-game/js/main.js
+++ first-sidescrolling-game/js/main.js
@@ -1,11 +1,12 @@
 gameScreen.classList.add(`hidden`)
+
 startButton.addEventListener(`click`, (e) => {
     e.target.classList.add(`hidden`);
     backGroundScreen.style.display = `none`;
     gameScreen.classList.remove(`hidden`)
 })
 //set movement speed
-let movementSpeed = 15;
+let movementSpeed = 25;
 function movedown() {
     spaceship.style.top = parseInt(spaceship.style.top) + movementSpeed + `px`;
 }
@@ -20,26 +21,51 @@ function moveright() {
     spaceship.style.left = parseInt(spaceship.style.left) + movementSpeed + `px`;
 }
 
+
 //add event listener to window
 //initialise allowed movement keys
-let allowedKeys = [`KeyA`, `KeyD`, `KeyW`, `KeyS`]
+let allowedKeys = [`KeyA`, `KeyD`, `KeyW`, `KeyS`, `Space`]
 window.addEventListener(`load`, () => {
     spaceship.style.left = 0;
     spaceship.style.top = 0;
 })
 window.addEventListener(`keydown`, (e) => {
+    console.log(e.code);
+    //dynamically get both gamescreen's height and width;
+    let gameScreenHeight = gameScreen.offsetHeight;
+    let gameScreenWidth = gameScreen.offsetWidth;
+
     if (allowedKeys.includes(e.code)) {
+        let leftPixelsParsed = spaceship.style.left
+        leftPixelsSpaceship = parseInt(leftPixelsParsed, 10)
+        let topPixelsParsed = spaceship.style.top
+        topPixelsSpaceship = parseInt(topPixelsParsed, 10)
         if (e.code === `KeyA`) {
-            moveleft()
+            if (leftPixelsSpaceship > 0) {
+                moveleft()
+            }
+
         }
         if (e.code === `KeyD`) {
-            moveright()
+            if (leftPixelsSpaceship < gameScreenWidth - spaceship.offsetWidth) {
+
+                moveright()
+            }
         }
         if (e.code === `KeyW`) {
-            moveup()
+
+            if (topPixelsSpaceship > 0) {
+                moveup()
+            }
         }
         if (e.code === `KeyS`) {
-            movedown()
+            console.log(topPixelsSpaceship);
+            console.log();
+            if (topPixelsSpaceship < gameScreenHeight - spaceship.offsetHeight)
+                movedown()
+
         }
     }
-})
+
+}
+)
